3a46fc4d4393293f18dd96073dbba88ba32fb3e6
David Lormor <davidlormor@gmail.com>
2016-02-20 10:00:07 -0600

    Set currentEncampment on index controller

--- app/index/route.js
+++ app/index/route.js
@@ -3,7 +3,27 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   geolocation: Ember.inject.service(),
   
-  getCurrentLocation: function() {
-    this.get('geolocation').getLocation();
-  }.on('activate')
+  beforeModel() {
+    this.getCurrentLocation();
+  },
+  
+  getCurrentLocation() {
+    var route = this;
+    
+    this.get('geolocation').getLocation().then(function(geoObject) {
+      route.joinOrCreateEncampment(geoObject);
+    });
+  },
+  
+  joinOrCreateEncampment(geoObject) {
+    var lat = geoObject.coords.latitude;
+    var lng = geoObject.coords.longitude;
+    
+    var currentEncampment = this.store.createRecord('encampment', {
+      lat: lat,
+      lng: lng
+    });
+    
+    this.controllerFor('index').set('currentEncampment', currentEncampment);
+  }
 });
