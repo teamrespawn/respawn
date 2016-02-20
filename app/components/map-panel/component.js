9546f1956fca12b91ef847bafde655a58e834543
David Lormor <davidlormor@gmail.com>
2016-02-20 07:50:20 -0600

    Get the map rendering

--- app/components/map-panel/component.js
+++ app/components/map-panel/component.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  geolocation: Ember.inject.service(),
+  
+  latitude: Ember.computed.alias('geolocation.geoObject.coords.latitude'),
+  longitude: Ember.computed.alias('geolocation.geoObject.coords.longitude'),
+  zoom: 10
 });
--- app/index/route.js
+++ app/index/route.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  geolocation: Ember.inject.service(),
+  
+  getCurrentLocation: function() {
+    this.get('geolocation').getLocation();
+  }.on('activate')
 });
