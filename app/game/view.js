0c9fa481b5a06c6b83be804a18b8a893ba34eb4f
Cayenne Dev Director <brasseale@Cayennes-MacBook-Pro.local>
2016-02-21 15:11:22 -0600

    Adding borked js

--- app/game/view.js
+++ app/game/view.js
@@ -1,21 +1,29 @@
 import Ember from 'ember';
 
 export default Ember.View.extend({
-  classNames: ['index'],
+  classNames: ['game'],
   didInsertElement: function () {
     storyResize();
     $(window).resize(function () {
       storyResize();
     });
-  },
-  classNames: ['game']
+  }
+  
 });
+function mapReplace($gifvid, $src) {
+  if($gifvid === "gif") {
 
+  } else {
+
+  }
+}
 function storyResize() {
   var wHeight = $(window).height();
   var spBtnHeight = $('.respawn-button').height();
   var searchHeight = $('.scouting-wrap').height();
-  var termHeight = wHeight - (spBtnHeight + searchHeight) - 40;
-  console.log(termHeight);
+  var termHeight = ( wHeight - (spBtnHeight + searchHeight) ) - 80;
+
+  var storyWidth = $('.story-feed').width() - 40;
   $('.terminal').css('max-height', termHeight);
+  $('.terminal').css('width', storyWidth);
 }
