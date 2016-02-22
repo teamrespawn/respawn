b10083f69999691ac28d0fe6ec1e795b23bb4e87
David Lormor <davidlormor@gmail.com>
2016-02-21 18:56:22 -0600

    More error handling

--- app/components/search-party-button/component.js
+++ app/components/search-party-button/component.js
@@ -2,9 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   searchPartySize: 0,
+  searchable: Ember.computed.gt('searchPartySize', 0),
   
   tagName: 'button',
   classNames: ['search-party-action'],
+  classNameBindings: ['searchable:enabled'],
   
   session: Ember.inject.service(),
   encampment: Ember.computed.alias('session.currentEncampment'),
@@ -13,8 +15,12 @@ export default Ember.Component.extend({
   click(e) {
     e.preventDefault();
     var partySize = this.get('searchPartySize');
-    this.get('messages').newTextMessage(`Sending out a search party of ${partySize} survivors...`);
-    this.get('encampment').collectResources(partySize);
-    this.sendAction('reset');
+    if(this.get('searchable')) {
+      this.get('messages').newTextMessage(`Sending out a search party of ${partySize} survivors...`);
+      this.get('encampment').collectResources(partySize);
+      this.sendAction('reset');
+    } else {
+      this.get('messages').newTextMessage("You haven't assigned any survivors to the search party...", "error");
+    }
   }
 });
